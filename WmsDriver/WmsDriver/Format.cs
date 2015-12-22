8d41971a46cc87005b58210057c12ded933d1fc5
Dominik Mikiewicz <dominik@cartomatic.pl>
2015-12-22 17:47:53 +0100

    Add some format utils

--- WmsDriver/WmsDriver/Format.cs
+++ WmsDriver/WmsDriver/Format.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Drawing.Imaging;
+
 namespace Cartomatic.Wms
 {
     public abstract partial class WmsDriver
     {
-        
+        /// <summary>
+        /// Used for setting up output format of image file
+        /// </summary>
+        protected internal ImageCodecInfo GetEncoderInfo(String mimeType)
+        {
+            return ImageCodecInfo.GetImageEncoders().FirstOrDefault(encoder => encoder.MimeType == mimeType || encoder.MimeType.Replace("image/", "") == mimeType);
+        }
     }
 }
