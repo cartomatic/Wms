42c24958caf35e0f931244a407dce25307274d9d
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-01-08 20:21:15 +0100

    Start GetCapabilities

---
+++ WmsDriverManifold/WmsDriver/HandleGetCapabilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartomatic.Manifold
+{
+    public partial class WmsDriver
+    {
+        
+    }
+}
